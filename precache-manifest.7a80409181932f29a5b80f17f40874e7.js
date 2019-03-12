17ef7e6644ddc7b9bac74a4f6cb1992c945c08f7
Geeeva <ivana.gilich@gmail.com>
2019-03-12 16:58:06 +0100

    Updates

---
+++ precache-manifest.7a80409181932f29a5b80f17f40874e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e85aec1f69f88a462959e415f5e8944",
+    "url": "/Geeeva/Weather-forecast/static/media/Oswald-Light.0e85aec1.ttf"
+  },
+  {
+    "revision": "3b4e9f81a27beb7b237d",
+    "url": "/Geeeva/Weather-forecast/static/js/runtime~main.60e0f566.js"
+  },
+  {
+    "revision": "cce39f1ebd38446464ae",
+    "url": "/Geeeva/Weather-forecast/static/js/main.724dd566.chunk.js"
+  },
+  {
+    "revision": "8f9f45d49f4bd39eaa17",
+    "url": "/Geeeva/Weather-forecast/static/js/2.69778955.chunk.js"
+  },
+  {
+    "revision": "cce39f1ebd38446464ae",
+    "url": "/Geeeva/Weather-forecast/static/css/main.dc03e879.chunk.css"
+  },
+  {
+    "revision": "3d3e5068892d913c5c8489ac7d2a7911",
+    "url": "/Geeeva/Weather-forecast/index.html"
+  }
+];
